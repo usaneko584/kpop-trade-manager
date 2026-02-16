316ffd45767febf0151f490aa73b90bc2b182c8a
usaneko584 <rkou2002@gmail.com>
2026-02-16 20:39:45 +0900

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class Trade(Base):
     __tablename__ = "trades"
 
     id = Column(Integer, primary_key=True, index=True)
-    # ★追加：LINEのユーザーIDを保存する列
     user_id = Column(String, index=True, nullable=False) 
     partner_name = Column(String, nullable=False)
     status = Column(String, default="交渉中")
