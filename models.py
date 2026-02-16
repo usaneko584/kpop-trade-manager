89e828ae2bd97a083b3db2d66b0b6c2e420492ce
usaneko584 <rkou2002@gmail.com>
2026-02-16 20:01:53 +0900

    Update models.py

--- models.py
+++ models.py
@@ -3,11 +3,10 @@ from database import Base
 
 class Trade(Base):
     __tablename__ = "trades"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)  # ★誰のデータか保存する列を追加
-    partner_name = Column(String, nullable=False)
 
     id = Column(Integer, primary_key=True, index=True)
+    # ★追加：LINEのユーザーIDを保存する列
+    user_id = Column(String, index=True, nullable=False) 
     partner_name = Column(String, nullable=False)
     status = Column(String, default="交渉中")
     give_item = Column(String)
